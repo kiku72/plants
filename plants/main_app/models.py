2aaccd186ea04f5c9ed8060136c7b31db10fc115
nkolotsos <nkolotsos@gmail.com>
2023-08-21 22:49:43 +1000

    Adding preliminary age calculation to model - plant

--- plants/main_app/models.py
+++ plants/main_app/models.py
@@ -9,7 +9,7 @@ class Plant(models.Model):
     name = models.CharField(max_length=20)
     date = models.DateField('Date Planted')
     description = models.TextField(max_length=500)
-    # age = models.IntegerField()
+    age = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -18,5 +18,24 @@ class Plant(models.Model):
     # New and updated plants redirect back to dashboard
     def get_absolute_url(self):
         return reverse('plants_list')
+
+    @property
+    def Get_age(self):
+        calc_age = date.today().year - self.date.year
+        date_stripped = str(calc_age).split(",", 1)[0]
+        return date_stripped
+
+    def save(self, *args, **kwargs):
+        self.age = self.Get_age
+        super(Plant, self).save(*args, **kwargs)
+    # @property
+    # def get_age(self):
+    #     return relativedelta(self.birth_date.days, datetime.date.now()).years
+     
+
+    # def save(self, *args, **kwargs):
+    #     self.unique_id = self.get_unique_id
+    #     self.age = self.get_age
+    #     super(Person, self).save(*args, **kwargs)
 
 # class Collection(models.Model):
