b00f4cc824893a5cb753d3fdf72654dc7cce36fc
kiku72 <128595283+kiku72@users.noreply.github.com>
2023-08-21 16:44:41 +1000

    Added update/delete functionality

--- plants/main_app/urls.py
+++ plants/main_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from main_app.views import PlantList
+from main_app.views import PlantList, PlantDetailView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('plants/', PlantList.as_view(), name='plants_list'),
-    # path('plants/<int:plant_id>/', views.plants_details.as_view(), name='detail'),
+    path('plants/<int:pk>/', views.PlantDetailView.as_view(), name='detail'),
     path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
+    path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plants_update'),
+    path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='plants_delete'),
 ]
--- plants/main_app/views.py
+++ plants/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Plant
 
 # Create your views here.
@@ -16,5 +16,16 @@ class PlantList (ListView):
 
 class PlantCreate (CreateView):
     model = Plant
-    # age is not included because it can be calculated using date-planted instead
+    # age is not included because it can be calculated with date-planted instead
     fields = ['name', 'date', 'description']
+
+class PlantDetailView (DetailView):
+    model = Plant
+
+class PlantUpdate (UpdateView):
+    model = Plant
+    fields = ['name', 'date', 'description']
+
+class PlantDelete (DeleteView):
+    model = Plant
+    success_url = '/plants'
