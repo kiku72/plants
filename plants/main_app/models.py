922264607c6df595d5a5a63d9e274f107c53bc6e
nkolotsos <nkolotsos@gmail.com>
2023-08-21 20:54:01 +1000

    Authentication and authorisation implemented

--- plants/main_app/models.py
+++ plants/main_app/models.py
@@ -9,7 +9,7 @@ class Plant(models.Model):
     name = models.CharField(max_length=20)
     date = models.DateField('Date Planted')
     description = models.TextField(max_length=500)
-    age = models.IntegerField()
+    # age = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- plants/main_app/views.py
+++ plants/main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import Plant
@@ -13,10 +15,14 @@ def home (request):
 def about (request):
     return render(request, 'about.html')
 
-class PlantList (ListView):
+class PlantList (LoginRequiredMixin, ListView):
     model = Plant
 
-class PlantCreate (CreateView):
+    # Filtering by user
+    def get_queryset(self):
+        return Plant.objects.filter(user=self.request.user)
+
+class PlantCreate (LoginRequiredMixin, CreateView):
     model = Plant
     # age is not included because it can be calculated using date-planted instead
     fields = ['name', 'date', 'description']
@@ -25,14 +31,14 @@ class PlantCreate (CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
     
-class PlantDetailView (DetailView):
+class PlantDetailView (LoginRequiredMixin, DetailView):
     model = Plant
 
-class PlantUpdate (UpdateView):
+class PlantUpdate (LoginRequiredMixin, UpdateView):
     model = Plant
     fields = ['name', 'date', 'description']
 
-class PlantDelete (DeleteView):
+class PlantDelete (LoginRequiredMixin, DeleteView):
     model = Plant
     success_url = '/plants'
 
