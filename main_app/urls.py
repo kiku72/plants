55d7b0bb805050166de6d16a599dfc23288d6147
nkolotsos <nkolotsos@gmail.com>
2023-08-25 11:37:09 +1000

    Explore stuff and polish

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from main_app.views import PlantList
+from main_app.views import PlantList, PlantExplore
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('plants/', PlantList.as_view(), name='plants_list'),
+    path('plants/explore', PlantExplore.as_view(), name='plants_explore'),
     path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
     path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
     path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plants_update'),
@@ -14,4 +17,4 @@ urlpatterns = [
     path('plants/<int:plant_id>/add_photo/', views.add_photo, name='add_photo'),
     path('plants/create_photo/', views.create_photo, name='create_photo'),
     path('plants/<int:plant_id>/add_comment', views.add_comment, name='add_comment'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
